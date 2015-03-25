5ad7634af4585bcccd2da7ce76890d3b2dc12480
artempolikarpov <websatart@gmail.com>
2015-03-25 21:10:53 +0200

    testClass.java added while cycle

--- src/com/selenium/testClass.java
+++ src/com/selenium/testClass.java
@@ -7,11 +7,18 @@ public class testClass {
 
     public static void main(String[] args) {
         //System.out.println("TestClass output");
-        if (args[0].equals("Hello") && args[1].equals("Class")) {
-            System.out.println("Success");
+//        if (args[0].equals("Hello") && args[1].equals("Class")) {
+//
+//            System.out.println("Success");
+//        }
+//        else {
+//            System.out.println("Good bye");
+//        }
+        int a = 5;
+        while (a < 15) {
+            System.out.println("a = " + a);
+            a++;
         }
-        else {
-            System.out.println("Good bye");
-        }
+        System.out.println("While is over");
     }
 }
