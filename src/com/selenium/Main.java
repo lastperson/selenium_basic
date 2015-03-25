bc8eae6b2acae9b01cd28c3f6730a0c5aa89a777
artempolikarpov <websatart@gmail.com>
2015-03-25 19:55:20 +0200

    first commit main.java added

---
+++ src/com/selenium/Main.java
@@ -0,0 +1,44 @@
+package com.selenium;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double c = 13.4;
+        int x=256;
+        int y=3180;
+        double d = 30;
+//        System.out.println("x + y = " +(x+y));
+//        System.out.println(Math.pow(d,2));
+//
+//        String str1 = "Selenium";
+//        String str2 = "java";
+//        System.out.println(str1+" "+str2);
+//        System.out.println("Sum of "+x+" and "+y+" = \n"+(x+y));
+//        System.out.println("\n180/30 = "+(180/30));
+
+//        int[]   arr = {1,2,3};
+//        System.out.println(Arrays.toString(arr));
+//
+//        int[] arr2 = {10,0,0,21,-5,38};
+////        System.out.println(arr2);
+//
+//        for (int in:arr2)
+//            System.out.print(in + " ");
+//
+//        System.out.println("");
+//
+//        for (int i=0; i<arr2.length; i++){
+//            System.out.print(arr2[i] + " ");
+//        }
+
+        String[] strArr1 = {"Teddy", "Arny", "Tommy"};
+        String[] strArr2 = {"Bear", "Schwarc", "The Gun"};
+
+        for (int i = 0; i < strArr1.length; i++) {
+            System.out.println(strArr1[i] + " " + strArr2[i]);
+        }
+
+    }
+}
