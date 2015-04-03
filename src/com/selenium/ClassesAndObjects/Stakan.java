7a392c9799ebf0fc74d9305b9d32921cc6138e69
artempolikarpov <artpol@ukr.net>
2015-04-03 13:36:36 +0300

    Stakan class hometask is finished

--- src/com/selenium/ClassesAndObjects/Stakan.java
+++ src/com/selenium/ClassesAndObjects/Stakan.java
@@ -6,7 +6,7 @@ public class Stakan {
 
     public String name;
     public static String material = "Bumaga";
-    public static int height;
+    public static int height = 10;
     public int fillPercent = 0;
 
     public Stakan (String name) {
@@ -18,14 +18,27 @@ public class Stakan {
     }
 
     public void fill (int fill) {
-        fill = this.fillPercent;
+//        this.fillPercent = this.fillPercent + fill;
+        this.fillPercent += fill;
+        if (this.fillPercent > 100) {
+            this.fillPercent = 100;
+            System.out.println(this.name +" perepolnen. Lishnee vylilos.");
+        }
     }
 
     public int pour (){
+        int vtemp = this.fillPercent;
         this.fillPercent = 0;
-        return this.fillPercent;
+        return vtemp;
     }
 
+    public String name (){
+        return this.name;
+    }
+
+    public static int volume(int v) {
+        return v*v;
+    }
 
 
 
